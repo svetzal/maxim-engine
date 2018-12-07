7cf10ae2bf7fbf6553aa67230b6a522150de1d23
Stacey Vetzal <stacey@vetzal.com>
2018-12-06 21:03:26 -0500

    looking to clean up a little duplication in tests

--- specs/engine_spec.js
+++ specs/engine_spec.js
@@ -9,7 +9,7 @@ const Engine = require('../src/engine');
 
 describe('Engine', () => {
 
-    let engine, readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble, dummyData;
+    let engine;
 
     describe('rule registration', () => {
 
@@ -46,103 +46,103 @@ describe('Engine', () => {
 
     });
 
-    describe('collaboration', () => {
+    function buildProxyBuilderDouble(dummyData) {
+        return {
+            reset: sinon.spy(),
+            wrap: sinon.stub().returns(dummyData),
+            getReferencedProperties: sinon.spy()
+        };
+    }
+
+    describe('proxy behaviour', () => {
+
+        let readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble, dummyData;
+
         beforeEach(() => {
             dummyData = { data: 'dummy' };
-            readOnlyProxyBuilderDouble = {
-                reset: sinon.spy(),
-                wrap: sinon.stub().returns(dummyData),
-                getReferencedProperties: sinon.spy()
-            };
-            writeThroughProxyBuilderDouble = {
-                reset: sinon.spy(),
-                wrap: sinon.stub().returns(dummyData),
-                getReferencedProperties: sinon.spy()
-            };
+            readOnlyProxyBuilderDouble = buildProxyBuilderDouble(dummyData);
+            writeThroughProxyBuilderDouble = buildProxyBuilderDouble(dummyData);
             engine = new Engine(readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble);
-
-            engine.register({
-                condition: wm => wm.data === "dummy",
-                consequence: wm => wm.data = "transformed"
-            });
-            engine.execute(dummyData, 2);
         });
 
-        describe('with ReadOnlyProxyBuilder', () => {
-            it('should reset referenced properties', () => {
-                expect(readOnlyProxyBuilderDouble.reset.callCount).to.equal(2);
+        describe('collaboration', () => {
+            beforeEach(() => {
+                engine.register({
+                    condition: wm => wm.data === "dummy",
+                    consequence: wm => wm.data = "transformed"
+                });
+                engine.execute(dummyData, 2);
             });
 
-            it('should wrap working memory after reset', () => {
-                expect(readOnlyProxyBuilderDouble.wrap.callCount).to.equal(2);
-                expect(readOnlyProxyBuilderDouble.reset.calledBefore(readOnlyProxyBuilderDouble.wrap)).to.be.true;
+            describe('with ReadOnlyProxyBuilder', () => {
+                it('should reset referenced properties', () => {
+                    expect(readOnlyProxyBuilderDouble.reset.callCount).to.equal(2);
+                });
+
+                it('should wrap working memory after reset', () => {
+                    expect(readOnlyProxyBuilderDouble.wrap.callCount).to.equal(2);
+                    expect(readOnlyProxyBuilderDouble.reset.calledBefore(readOnlyProxyBuilderDouble.wrap)).to.be.true;
+                });
+
+                it('should provide referenced properties', () => {
+                    expect(readOnlyProxyBuilderDouble.getReferencedProperties.callCount).to.equal(2);
+                });
             });
 
-            it('should provide referenced properties', () => {
-                expect(readOnlyProxyBuilderDouble.getReferencedProperties.callCount).to.equal(2);
+            describe('with writeThroughProxyBuilder, calls made twice', () => {
+                it('should reset referenced properties', () => {
+                    expect(writeThroughProxyBuilderDouble.reset.callCount).to.equal(1);
+                });
+
+                it('should wrap working memory after reset', () => {
+                    expect(writeThroughProxyBuilderDouble.wrap.callCount).to.equal(1);
+                    expect(writeThroughProxyBuilderDouble.reset.calledBefore(writeThroughProxyBuilderDouble.wrap)).to.be.true;
+                });
+
+                it('should show 2 referenced properties calls because condition property was mutated', () => {
+                    expect(writeThroughProxyBuilderDouble.getReferencedProperties.callCount).to.equal(1);
+                });
             });
         });
 
-        describe('with writeThroughProxyBuilder, calls made twice', () => {
-            it('should reset referenced properties', () => {
-                expect(writeThroughProxyBuilderDouble.reset.callCount).to.equal(1);
-            });
+        describe('prioritization', () => {
 
-            it('should wrap working memory after reset', () => {
-                expect(writeThroughProxyBuilderDouble.wrap.callCount).to.equal(1);
-                expect(writeThroughProxyBuilderDouble.reset.calledBefore(writeThroughProxyBuilderDouble.wrap)).to.be.true;
-            });
+            let engine, func1, func2, dummyData, readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble;
 
-            it('should show 2 referenced properties calls because condition property was mutated', () => {
-                expect(writeThroughProxyBuilderDouble.getReferencedProperties.callCount).to.equal(1);
-            });
-        });
-    });
+            beforeEach(() => {
+                func1 = sinon.spy();
+                func2 = sinon.spy();
 
-    describe('prioritization', () => {
+                dummyData = { func1: func1, func2: func2 };
+                readOnlyProxyBuilderDouble = buildProxyBuilderDouble(dummyData);
+                writeThroughProxyBuilderDouble = buildProxyBuilderDouble(dummyData);
+                engine = new Engine(readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble);
 
-        let engine, func1, func2, dummyData, readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble;
-
-        beforeEach(() => {
-            func1 = sinon.spy();
-            func2 = sinon.spy();
-
-            dummyData = { func1: func1, func2: func2 };
-            readOnlyProxyBuilderDouble = {
-                reset: sinon.spy(),
-                wrap: sinon.stub().returns(dummyData),
-                getReferencedProperties: sinon.spy()
-            };
-            writeThroughProxyBuilderDouble = {
-                reset: sinon.spy(),
-                wrap: sinon.stub().returns(dummyData),
-                getReferencedProperties: sinon.spy()
-            };
-            engine = new Engine(readOnlyProxyBuilderDouble, writeThroughProxyBuilderDouble);
-
-            engine.register([
-                {
-                    priority: 1,
-                    condition: wm => true,
-                    consequence: wm => {
-                        wm.func1();
-                        return wm;
+                engine.register([
+                    {
+                        priority: 1,
+                        condition: wm => true,
+                        consequence: wm => {
+                            wm.func1();
+                            return wm;
+                        }
+                    },
+                    {
+                        priority: 2,
+                        condition: wm => true,
+                        consequence: wm => {
+                            wm.func2();
+                            return wm;
+                        }
                     }
-                },
-                {
-                    priority: 2,
-                    condition: wm => true,
-                    consequence: wm => {
-                        wm.func2();
-                        return wm;
-                    }
-                }
-            ]);
-        });
+                ]);
+            });
 
-        it('should respect priority', () => {
-            engine.execute({}, 1);
-            expect(func2.calledBefore(func1)).to.be.true;
+            it('should respect priority', () => {
+                engine.execute({}, 1);
+                expect(func2.calledBefore(func1)).to.be.true;
+            });
+
         });
 
     });
